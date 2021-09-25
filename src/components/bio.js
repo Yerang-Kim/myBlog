cc40dffaf92a38bae7918a03d1dcb3e5571739b4
yerang <grondin@naver.com>
2021-09-25 17:45:12 +0900

    change link from twitter to github

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,11 +44,13 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          Written by <a href={`https://github.com/${social?.twitter || ``}`}>
+          <strong>{author.name}</strong> {author?.summary || null}
           </a>
+          {` `}
+          {/* <a href={`https://github.com/${social?.twitter || ``}`}>
+            You should follow them on Twitter
+          </a> */}
         </p>
       )}
     </div>
